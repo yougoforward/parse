3a1d92ec0dc2263f334a4486781d89ebd4688720
yougoforward <908865817@qq.com>
2020-12-06 17:39:52 +0800

    base

--- experiments/segmentation/scripts/abrnet_pperson.sh
+++ experiments/segmentation/scripts/abrnet_pperson.sh
@@ -1,15 +1,15 @@
 # !/usr/bin/env bash
 # train
 python -m experiments.segmentation.train_parse --dataset pperson \
-    --model abrnet --aux --base-size 520 --crop-size 520 \
+    --model abrnet --aux --base-size 473 --crop-size 473 \
     --backbone resnet101 --checkname abrnet_res101_pperson --dilated --epochs 150 --batch-size 20 --lr 0.01
 
 #test [single-scale]
 python -m experiments.segmentation.test_whole --dataset pperson \
-    --model abrnet --aux --base-size 520 --crop-size 520 \
+    --model abrnet --aux --base-size 473 --crop-size 473 \
     --backbone resnet101 --resume experiments/segmentation/runs/pperson/abrnet/abrnet_res101_pperson/model_best.pth.tar --split val --mode testval --dilated
 
 #test [multi-scale]
 python -m experiments.segmentation.test_whole --dataset pperson \
-    --model abrnet --aux --base-size 520 --crop-size 520 \
+    --model abrnet --aux --base-size 473 --crop-size 473 \
     --backbone resnet101 --resume experiments/segmentation/runs/pperson/abrnet/abrnet_res101_pperson/model_best.pth.tar --split val --mode testval --ms --dilated
