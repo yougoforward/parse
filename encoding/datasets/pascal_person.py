a07518d33d021a9de776562b2b9de423e3df22ec
yougoforward <908865817@qq.com>
2020-12-06 21:37:51 +0800

    base

--- encoding/datasets/pascal_person.py
+++ encoding/datasets/pascal_person.py
@@ -143,9 +143,9 @@ class PersonSegmentation(BaseDataset):
         img = img.crop((x1, y1, x1+crop_size, y1+crop_size))
         mask = mask.crop((x1, y1, x1+crop_size, y1+crop_size))
         # gaussian blur as in PSP
-        if random.random() < 0.5:
-            img = img.filter(ImageFilter.GaussianBlur(
-                radius=random.random()))
+        # if random.random() < 0.5:
+        #     img = img.filter(ImageFilter.GaussianBlur(
+        #         radius=random.random()))
         # final transform
         return img, self._mask_transform(mask)
 
