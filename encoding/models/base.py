6d2e406731d51d40bec30f6c6bac70f7d0e42301
yougoforward <908865817@qq.com>
2020-12-06 10:51:20 +0800

    base

--- encoding/models/base.py
+++ encoding/models/base.py
@@ -19,8 +19,80 @@ from ..utils import batch_pix_accuracy, batch_intersection_union
 
 up_kwargs = {'mode': 'bilinear', 'align_corners': True}
 
-__all__ = ['BaseNet', 'MultiEvalModule']
+__all__ = ['BaseNet', 'MultiEvalModule', 'MultiEvalModule_whole']
+def get_backbone(name, **kwargs):
+    models = {
+        # resnet
+        'resnet50': resnet.resnet50,
+        'resnet101': resnet.resnet101,
+        'resnet152': resnet.resnet152,
+        # resnest
+        'resnest50': resnet.resnest50,
+        'resnest101': resnet.resnest101,
+        'resnest200': resnet.resnest200,
+        'resnest269': resnet.resnest269,
+        }
+    name = name.lower()
+    if name not in models:
+        raise ValueError('%s\n\t%s' % (str(name), '\n\t'.join(sorted(models.keys()))))
+    net = models[name](**kwargs)
+    return net
+# class BaseNet(nn.Module):
+#     def __init__(self, nclass, backbone, aux, se_loss, dilated=False, norm_layer=None,
+#                  base_size=520, crop_size=480, mean=[.485, .456, .406],
+#                  std=[.229, .224, .225], root='~/.encoding/models', *args, **kwargs):
+#         super(BaseNet, self).__init__()
+#         self.nclass = nclass
+#         self.aux = aux
+#         self.se_loss = se_loss
+#         self.mean = mean
+#         self.std = std
+#         self.base_size = base_size
+#         self.crop_size = crop_size
+#         # copying modules from pretrained models
+#         self.backbone = backbone
 
+#         self.pretrained = get_backbone(backbone, pretrained=True, dilated=dilated,
+#                                        norm_layer=norm_layer, root=root,
+#                                        *args, **kwargs)
+#         self.pretrained.fc = None
+#         self._up_kwargs = up_kwargs
+
+#     def base_forward(self, x):
+#         if self.backbone.startswith('wideresnet'):
+#             x = self.pretrained.mod1(x)
+#             x = self.pretrained.pool2(x)
+#             x = self.pretrained.mod2(x)
+#             x = self.pretrained.pool3(x)
+#             x = self.pretrained.mod3(x)
+#             x = self.pretrained.mod4(x)
+#             x = self.pretrained.mod5(x)
+#             c3 = x.clone()
+#             x = self.pretrained.mod6(x)
+#             x = self.pretrained.mod7(x)
+#             x = self.pretrained.bn_out(x)
+#             return None, None, c3, x
+#         else:
+#             x = self.pretrained.conv1(x)
+#             x = self.pretrained.bn1(x)
+#             x = self.pretrained.relu(x)
+#             x = self.pretrained.maxpool(x)
+#             c1 = self.pretrained.layer1(x)
+#             c2 = self.pretrained.layer2(c1)
+#             c3 = self.pretrained.layer3(c2)
+#             c4 = self.pretrained.layer4(c3)
+#         return c1, c2, c3, c4
+
+#     def evaluate(self, x, target=None):
+#         pred = self.forward(x)
+#         if isinstance(pred, (tuple, list)):
+#             pred = pred[0]
+#         if target is None:
+#             return pred
+#         correct, labeled = batch_pix_accuracy(pred.data, target.data)
+#         inter, union = batch_intersection_union(pred.data, target.data, self.nclass)
+#         return correct, labeled, inter, union
+    
 class BaseNet(nn.Module):
     def __init__(self, nclass, backbone, aux, se_loss, jpu=True, dilated=False, norm_layer=None,
                  base_size=520, crop_size=480, mean=[.485, .456, .406],
@@ -34,20 +106,23 @@ class BaseNet(nn.Module):
         self.base_size = base_size
         self.crop_size = crop_size
         # copying modules from pretrained models
-        if backbone == 'resnet50':
-            self.pretrained = resnet.resnet50(pretrained=True, dilated=dilated,
-                                              norm_layer=norm_layer, root=root)
-        elif backbone == 'resnet101':
-            self.pretrained = resnet.resnet101(pretrained=True, dilated=dilated,
-                                               norm_layer=norm_layer, root=root)
-        elif backbone == 'resnet152':
-            self.pretrained = resnet.resnet152(pretrained=True, dilated=dilated,
-                                               norm_layer=norm_layer, root=root)
-        else:
-            raise RuntimeError('unknown backbone: {}'.format(backbone))
+        # if backbone == 'resnet50':
+        #     self.pretrained = resnet.resnet50(pretrained=True, dilated=dilated,
+        #                                       norm_layer=norm_layer, root=root)
+        # elif backbone == 'resnet101':
+        #     self.pretrained = resnet.resnet101(pretrained=True, dilated=dilated,
+        #                                        norm_layer=norm_layer, root=root)
+        # elif backbone == 'resnet152':
+        #     self.pretrained = resnet.resnet152(pretrained=True, dilated=dilated,
+        #                                        norm_layer=norm_layer, root=root)
+        # else:
+        #     raise RuntimeError('unknown backbone: {}'.format(backbone))
         # bilinear upsample options
         self._up_kwargs = up_kwargs
         self.backbone = backbone
+        self.pretrained = get_backbone(backbone, pretrained=True, dilated=dilated, norm_layer=norm_layer, root=root)
+        self.pretrained.fc = None
+        
         self.jpu = None
         if jpu == 'JPU':
             self.jpu = JPU([512, 1024, 2048], width=512, norm_layer=norm_layer, up_kwargs=up_kwargs)
@@ -79,6 +154,76 @@ class BaseNet(nn.Module):
         inter, union = batch_intersection_union(pred.data, target.data, self.nclass)
         return correct, labeled, inter, union
 
+class MultiEvalModule_whole(DataParallel):
+    """Multi-size Segmentation Eavluator"""
+    def __init__(self, module, nclass, device_ids=None, flip=True,
+                 scales=[0.5, 0.75, 1.0, 1.25, 1.5, 1.75]):
+        super(MultiEvalModule_whole, self).__init__(module, device_ids)
+        self.nclass = nclass
+        self.base_size = module.base_size
+        self.crop_size = module.crop_size
+        self.scales = scales
+        self.flip = flip
+        print('MultiEvalModule: base_size {}, crop_size {}'. \
+            format(self.base_size, self.crop_size))
+
+    def parallel_forward(self, inputs, **kwargs):
+        """Multi-GPU Mult-size Evaluation
+
+        Args:
+            inputs: list of Tensors
+        """
+        inputs = [(input.unsqueeze(0).cuda(device),)
+                  for input, device in zip(inputs, self.device_ids)]
+        replicas = self.replicate(self, self.device_ids[:len(inputs)])
+        kwargs = []
+        if len(inputs) < len(kwargs):
+            inputs.extend([() for _ in range(len(kwargs) - len(inputs))])
+        elif len(kwargs) < len(inputs):
+            kwargs.extend([{} for _ in range(len(inputs) - len(kwargs))])
+        outputs = self.parallel_apply(replicas, inputs, kwargs)
+        #for out in outputs:
+        #    print('out.size()', out.size())
+        return outputs
+
+    def forward(self, image):
+        """Mult-size Evaluation"""
+        # only single image is supported for evaluation
+        batch, _, h, w = image.size()
+        assert(batch == 1)
+        stride_rate = 2.0/3.0
+        crop_size = self.crop_size
+        stride = int(crop_size * stride_rate)
+        with torch.cuda.device_of(image):
+            scores = image.new().resize_(batch,self.nclass,h,w).zero_().cuda()
+
+        for scale in self.scales:
+            long_size = int(math.ceil(self.base_size * scale))
+            if h > w:
+                height = long_size
+                width = int(1.0 * w * long_size / h + 0.5)
+                short_size = width
+            else:
+                width = long_size
+                height = int(1.0 * h * long_size / w + 0.5)
+                short_size = height
+            # resize image to current size
+            cur_img = resize_image(image, height, width, **self.module._up_kwargs)
+
+            if short_size < crop_size:
+                    # pad if needed
+                    pad_img = pad_image(cur_img, self.module.mean,
+                                        self.module.std, crop_size)
+            else:
+                pad_img = cur_img
+            _,_,ph,pw = pad_img.size()
+            outputs = module_inference(self.module, pad_img, self.flip)
+            outputs = crop_image(outputs, 0, height, 0, width)
+
+            score = resize_image(outputs, h, w, **self.module._up_kwargs)
+            scores += score
+
+        return scores
 
 class MultiEvalModule(DataParallel):
     """Multi-size Segmentation Eavluator"""
